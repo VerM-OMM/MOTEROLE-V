afa710c43f975fa7195d1137163470161c8542db
OliverMolina <omolina.k11832651@umak.edu.ph>
2024-10-14 19:07:26 +0800

    safe line static import

--- src/pages/Line.jsx
+++ src/pages/Line.jsx
@@ -7,26 +7,48 @@ import { LuArrowBigLeft } from 'react-icons/lu'
 import { PiGearSixBold } from 'react-icons/pi'
 import { IoBulbOutline } from 'react-icons/io5'
 
+// Static imports for all images
+import bghiga from '../assets/lineCategory/bghiga.png'
+import pahiga from '../assets/lineCategory/pahiga.png'
+import bghilis from '../assets/lineCategory/bghilis.png'
+import pahilis from '../assets/lineCategory/pahilis.png'
+import bgkurba from '../assets/lineCategory/bgkurba.png'
+import pakurba from '../assets/lineCategory/pakurba.png'
+import bgpatayo from '../assets/lineCategory/bgtayo.png'
+import patayo from '../assets/lineCategory/patayo.png'
+import bgzigzag from '../assets/lineCategory/bgzigzag.png'
+import pazigzag from '../assets/lineCategory/pazigzag.png'
+
+const lineImages = {
+    Patayo: { bg: bgpatayo, img: patayo },
+    Pahilis: { bg: bghilis, img: pahilis },
+    Pahiga: { bg: bghiga, img: pahiga },
+    Pakurba: { bg: bgkurba, img: pakurba },
+    Pazigzag: { bg: bgzigzag, img: pazigzag },
+}
+
 const Line = () => {
+    const lineTypes = ['Patayo', 'Pahilis', 'Pahiga', 'Pakurba', 'Pazigzag']
+
     useEffect(() => {
         document.title = 'Line'
-    })
+    }, [])
+
     return (
         <>
             <Background />
             <div className="flex h-screen justify-between p-5">
                 {/* left column */}
                 <div className="w-1/10 flex flex-col justify-between">
-                    {/* Action button acting as a "Back" button */}
                     <Actionbtn
                         text=""
                         to="/category"
                         bgColor="#F40000"
                         icon={LuArrowBigLeft}
                     />
-                    {/* No need to pass onClick if using the default navigate(-1) */}
                     <FullScreen />
                 </div>
+
                 {/* center */}
                 <div className="flex w-full flex-col items-center justify-center font-bubbles text-white ipad:overflow-hidden">
                     <div className="text-shadow relative flex h-[70%] w-[80%] justify-center rounded-3xl border-8 border-limblue bg-white p-8 mobile:h-[85%] mobile:border-4 mobile:p-4 ipad:h-[60%] ipad:p-6">
@@ -34,52 +56,34 @@ const Line = () => {
                             Linya
                         </span>
                         <div className="inner-shadow flex h-full w-full items-center justify-evenly space-x-4 overflow-x-auto rounded-2xl border-[0.5px] border-softgray bg-cheese p-4 mobile:overflow-x-auto mobile:rounded-xl ipad:overflow-x-auto">
-                            <Link
-                                to="/leveldifficulty"
-                                className="text-shadow h-[80%] w-1/4 flex-shrink-0 rounded-2xl border-8 border-limblue bg-butter mobile:h-[90%] mobile:w-1/3 mobile:border-4 ipad:w-60"
-                            ></Link>
-                            <Link
-                                to="/leveldifficulty"
-                                className="text-shadow h-[80%] w-1/4 flex-shrink-0 rounded-2xl border-8 border-limblue bg-butter mobile:h-[90%] mobile:w-1/3 mobile:border-4 ipad:w-60"
-                            ></Link>
-                            <Link
-                                to="/leveldifficulty"
-                                className="text-shadow h-[80%] w-1/4 flex-shrink-0 rounded-2xl border-8 border-limblue bg-butter mobile:h-[90%] mobile:w-1/3 mobile:border-4 ipad:w-60"
-                            ></Link>
-                            <Link
-                                to="/leveldifficulty"
-                                className="text-shadow h-[80%] w-1/4 flex-shrink-0 rounded-2xl border-8 border-limblue bg-butter mobile:h-[90%] mobile:w-1/3 mobile:border-4 ipad:w-60"
-                            ></Link>
-                            <Link
-                                to="/leveldifficulty"
-                                className="text-shadow h-[80%] w-1/4 flex-shrink-0 rounded-2xl border-8 border-limblue bg-butter mobile:h-[90%] mobile:w-1/3 mobile:border-4 ipad:w-60"
-                            ></Link>
-                            <Link
-                                to="/leveldifficulty"
-                                className="text-shadow h-[80%] w-1/4 flex-shrink-0 rounded-2xl border-8 border-limblue bg-butter mobile:h-[90%] mobile:w-1/3 mobile:border-4 ipad:w-60"
-                            ></Link>
-                            <Link
-                                to="/leveldifficulty"
-                                className="text-shadow h-[80%] w-1/4 flex-shrink-0 rounded-2xl border-8 border-limblue bg-butter mobile:h-[90%] mobile:w-1/3 mobile:border-4 ipad:w-60"
-                            ></Link>
-                            <Link
-                                to="/leveldifficulty"
-                                className="text-shadow h-[80%] w-1/4 flex-shrink-0 rounded-2xl border-8 border-limblue bg-butter mobile:h-[90%] mobile:w-1/3 mobile:border-4 ipad:w-60"
-                            ></Link>
-                            <Link
-                                to="/leveldifficulty"
-                                className="text-shadow h-[80%] w-1/4 flex-shrink-0 rounded-2xl border-8 border-limblue bg-butter mobile:h-[90%] mobile:w-1/3 mobile:border-4 ipad:w-60"
-                            ></Link>
-                            <Link
-                                to="/leveldifficulty"
-                                className="text-shadow h-[80%] w-1/4 flex-shrink-0 rounded-2xl border-8 border-limblue bg-butter mobile:h-[90%] mobile:w-1/3 mobile:border-4 ipad:w-60"
-                            ></Link>
+                            {lineTypes.map((lineType, index) => {
+                                const images = lineImages[lineType]
+                                return (
+                                    <Link
+                                        key={index}
+                                        to="/leveldifficulty"
+                                        className="text-shadow just flex h-[80%] w-1/4 flex-shrink-0 flex-col justify-center rounded-2xl border-8 border-limblue bg-butter bg-cover bg-center p-5 font-nunito text-4xl font-black text-black mobile:h-[90%] mobile:w-1/3 mobile:border-4 mobile:p-2 mobile:text-2xl ipad:w-60 ipad:p-3 ipad:text-3xl"
+                                        style={{
+                                            backgroundImage: `url(${images.bg})`,
+                                        }}
+                                    >
+                                        <img
+                                            src={images.img}
+                                            alt={lineType}
+                                            className="h-full w-full object-cover"
+                                        />
+                                        <div className="text-center">
+                                            {lineType}
+                                        </div>
+                                    </Link>
+                                )
+                            })}
                         </div>
                     </div>
                 </div>
+
                 {/* right column */}
                 <div className="w-1/10 flex select-none flex-col space-y-4 mobile:space-y-3">
-                    {/* Action button acting as a "Back" button */}
                     <Actionbtn
                         text=""
                         to="/settings"
